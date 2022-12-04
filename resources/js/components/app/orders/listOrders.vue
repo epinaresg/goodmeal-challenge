fc1395bd80acaf85f5c16f236006e39f914a2190
Elvis Pinares <epinaresg@gmail.com>
2022-12-04 15:03:13 -0500

    App - Mensaje de sin productos y sin ordenes

--- resources/js/components/app/orders/listOrders.vue
+++ resources/js/components/app/orders/listOrders.vue
@@ -10,7 +10,12 @@ export default {
 </script>
 
 <template>
-    <div class="orderContainer">
+    <div v-if="orders.length == 0" class="orderContainer">
+        <div class="pt-4 text-center">
+            <strong> No tiene ordenes registradas.</strong>
+        </div>
+    </div>
+    <div v-else class="orderContainer">
         <div class="py-3" v-for="order in orders" :key="order.id">
             <div class="position-relative orderItem py-3 px-4">
                 <OrderItem :order="order" />
--- resources/js/components/app/showStore/listProducts.vue
+++ resources/js/components/app/showStore/listProducts.vue
@@ -11,46 +11,54 @@ export default {
 
 <template>
     <div class="listProductsContainer">
-        <div class="categoryTabsContainer">
-            <div class="slideContainer">
-                <div
-                    class="nav nav-pills mb-2"
-                    id="v-pills-tab"
-                    role="tablist"
-                    aria-orientation="horizontal"
-                >
-                    <a
-                        v-for="(product, key) in products"
-                        :key="product.category_id"
-                        class="nav-link"
-                        :class="key == 0 ? 'active' : ''"
-                        :id="'v-' + product.category_id + '-tab'"
-                        data-toggle="pill"
-                        :href="'#v-' + product.category_id"
-                        role="tab"
-                        >{{ product.category_name }}</a
+        <div
+            v-if="products.length == 0"
+            class="categoryTabsContainer pt-4 text-center"
+        >
+            <strong> No tiene productos registrados.</strong>
+        </div>
+        <div v-else>
+            <div class="categoryTabsContainer">
+                <div class="slideContainer">
+                    <div
+                        class="nav nav-pills mb-2"
+                        id="v-pills-tab"
+                        role="tablist"
+                        aria-orientation="horizontal"
                     >
+                        <a
+                            v-for="(product, key) in products"
+                            :key="product.category_id"
+                            class="nav-link"
+                            :class="key == 0 ? 'active' : ''"
+                            :id="'v-' + product.category_id + '-tab'"
+                            data-toggle="pill"
+                            :href="'#v-' + product.category_id"
+                            role="tab"
+                            >{{ product.category_name }}</a
+                        >
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="productItemsContainer px-3 mt-2">
-            <div class="tab-content mt-2" id="v-pills-tabContent">
-                <div
-                    v-for="(product, key) in products"
-                    :key="product.category_id"
-                    class="tab-pane fade show"
-                    :class="key == 0 ? 'active' : ''"
-                    :id="'v-' + product.category_id"
-                    role="tabpanel"
-                >
-                    <div class="row w-100 mx-auto">
-                        <div
-                            class="col-lg-3 col-md-4 col-6 px-2 mt-2 mb-3"
-                            v-for="item in product.items"
-                            :key="item.id"
-                        >
-                            <ProductItem :store="store" :item="item" />
+            <div class="productItemsContainer px-3 mt-2">
+                <div class="tab-content mt-2" id="v-pills-tabContent">
+                    <div
+                        v-for="(product, key) in products"
+                        :key="product.category_id"
+                        class="tab-pane fade show"
+                        :class="key == 0 ? 'active' : ''"
+                        :id="'v-' + product.category_id"
+                        role="tabpanel"
+                    >
+                        <div class="row w-100 mx-auto">
+                            <div
+                                class="col-lg-3 col-md-4 col-6 px-2 mt-2 mb-3"
+                                v-for="item in product.items"
+                                :key="item.id"
+                            >
+                                <ProductItem :store="store" :item="item" />
+                            </div>
                         </div>
                     </div>
                 </div>
