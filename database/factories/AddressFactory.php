8c1285912129baa24eaabc69e61e357110f094e8
Elvis Pinares <epinaresg@gmail.com>
2022-12-04 19:00:26 -0500

    App API - Agregando factories

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'address' => fake()->address(),
+            'latitude' => fake()->latitude(),
+            'longitude' => fake()->longitude()
+        ];
+    }
+}
